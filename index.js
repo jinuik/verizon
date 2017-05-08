2821b7484e1b09e8aa4c1ca9942d05ea2537e995
Jinu Kuruvilla <jinu.kuruvilla@BRILLIO.COM>
2017-05-08 16:28:19 +0530

    alexa-app

--- index.js
+++ index.js
@@ -34,13 +34,68 @@ alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", 
 alexaApp.intent("nameIntent", {
     "slots": { "NAME": "LITERAL" },
     "utterances": [
-      "my {name is|name's} {names|NAME}", "set my name to {names|NAME}"
+      "HelloWorldIntent hello",
+"HelloWorldIntent say hello",
+"HelloWorldIntent say hello world"
     ]
   },
   function(request, response) {
-    response.say("Success!");
+    response.say("hi Welcome to brillio");
   }
 );
 
 app.listen(PORT);
 console.log("Listening on port " + PORT + ", try http://localhost:" + PORT + "/test");
+
+
+
+
+//'use strict';
+//var Alexa = require("alexa-sdk");
+//var http = require('http');
+//
+//http.createServer(function (request, response) {
+//   // Send the HTTP header 
+//   // HTTP Status: 200 : OK
+//   // Content Type: text/plain
+//    
+//     console.log('favicon requested request.url',request.url);
+//    if (request.url === '/favicon.ico') {
+//    response.writeHead(200, {'Content-Type': 'image/x-icon'} );
+//    response.end();
+//    console.log('favicon requested');
+//    return;
+//  }
+//
+//        if (request.method === 'GET' && request.url === '/') {
+//       response.writeHead(200, {'Content-Type': 'text/plain'});
+//   
+//   // Send the response body as "Hello World"
+//    console.log('favicon requested');
+//   response.end('Hello World\n');
+//  } 
+//   
+//
+//   
+//}).listen(process.env.PORT || 8000);
+//
+// exports.handler = function(event, context, callback) {
+//     console.log('event')
+//    var alexa = Alexa.handler(event, context);
+//    alexa.registerHandlers(handlers);
+//    alexa.execute();
+//};
+//
+//var handlers = {
+//    'LaunchRequest': function () {
+//        this.emit('SayHello');
+//    },
+//    'HelloWorldIntent': function () {
+//        this.emit('SayHello')
+//    },
+//    'SayHello': function () {
+//        this.emit(':tell', 'Hi Welcome to brillio!');
+//    }
+//};
+//
+//
