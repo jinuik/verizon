376f18b449aae533d4091c4b1e435fa8d523c8e2
Jinu Kuruvilla <jinu.kuruvilla@BRILLIO.COM>
2017-05-08 16:35:54 +0530

    alexa-app

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var express = require("express");
+ar express = require("express");
 var alexa = require("alexa-app");
 
 var PORT = process.env.port || 8080;
 var app = express();
 
 // ALWAYS setup the alexa app and attach it to express before anything else.
-var alexaApp = new alexa.app("test");
+var alexaApp = new alexa.app("brillioalexaapp");
 
 alexaApp.express({
   expressApp: app,
@@ -34,68 +34,13 @@ alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", 
 alexaApp.intent("nameIntent", {
     "slots": { "NAME": "LITERAL" },
     "utterances": [
-      "HelloWorldIntent hello",
-"HelloWorldIntent say hello",
-"HelloWorldIntent say hello world"
+      "my {name is|name's} {names|NAME}", "set my name to {names|NAME}"
     ]
   },
   function(request, response) {
-    response.say("hi Welcome to brillio");
+    response.say("Success!");
   }
 );
 
 app.listen(PORT);
 console.log("Listening on port " + PORT + ", try http://localhost:" + PORT + "/test");
-
-
-
-
-//'use strict';
-//var Alexa = require("alexa-sdk");
-//var http = require('http');
-//
-//http.createServer(function (request, response) {
-//   // Send the HTTP header 
-//   // HTTP Status: 200 : OK
-//   // Content Type: text/plain
-//    
-//     console.log('favicon requested request.url',request.url);
-//    if (request.url === '/favicon.ico') {
-//    response.writeHead(200, {'Content-Type': 'image/x-icon'} );
-//    response.end();
-//    console.log('favicon requested');
-//    return;
-//  }
-//
-//        if (request.method === 'GET' && request.url === '/') {
-//       response.writeHead(200, {'Content-Type': 'text/plain'});
-//   
-//   // Send the response body as "Hello World"
-//    console.log('favicon requested');
-//   response.end('Hello World\n');
-//  } 
-//   
-//
-//   
-//}).listen(process.env.PORT || 8000);
-//
-// exports.handler = function(event, context, callback) {
-//     console.log('event')
-//    var alexa = Alexa.handler(event, context);
-//    alexa.registerHandlers(handlers);
-//    alexa.execute();
-//};
-//
-//var handlers = {
-//    'LaunchRequest': function () {
-//        this.emit('SayHello');
-//    },
-//    'HelloWorldIntent': function () {
-//        this.emit('SayHello')
-//    },
-//    'SayHello': function () {
-//        this.emit(':tell', 'Hi Welcome to brillio!');
-//    }
-//};
-//
-//
