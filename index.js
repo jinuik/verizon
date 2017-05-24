1f74f3dd93c48335d5d14465e1e9c0d51fbac117
Jinu Kuruvilla <jinu.kuruvilla@BRILLIO.COM>
2017-05-24 11:20:09 +0530

    Jinu

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ alexaApp.intent("nameIntent", {
     ]
   },
   function(request, response) {
-    console.log(request);
+    console.log(request.data.request.intent);
     console.log('hitting this page')
     response.say("My name is Jinu");
   }
